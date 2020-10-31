2495d6f5ee715b38d0aeaa3b06ac1a85b2606235
Elisha Hollander <just4now666666@gmail.com>
2020-10-31 18:09:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time, sys
+import time
 from math import pi as RealPi
-l = [2, 0, 67921, 0, 2919, 10, 3, 11, 3, 1000] #bigger l[4] will cause the recursion to breaak the operation, bigger l[9] will cause a very long calculation
-l[1] = l[3] = int(l[9] ** (7 / 3)) #a fancy way of saying that pi1 and pi3 are also too slow
-sys.setrecursionlimit(l[4] + 6)
+l = [2, 0, 67921, 0, 0, 10, 3, 11, 3, 1000] #bigger l[9] will cause a very long calculation
+l[1] = l[3] = l[4] = int(l[9] ** 2) #a fancy way of saying that pi1, pi3 and pi4 are also too slow
 a, b = '{}'
 for i in range(0,10):exec(f'from pi{i} import pi as pi{i};t = time.time();pi = pi{i}(l[i]);print(f"pi{i}.py will calculate pi in {a}1000 * (time.time() - t){b} ms, with a mistake of {a}abs(RealPi - pi){b}")')
