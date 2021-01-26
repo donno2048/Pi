8f3568696afd527900e6e54f9a818b629384e1c9
Elisha Hollander <just4now666666@gmail.com>
2021-01-27 00:20:26 +0200

    Twice faster

--- pi1.py
+++ pi1.py
@@ -1,1 +1,1 @@
-def pi(precision):return 4 * sum([(-1) ** j / (2 * j + 1) for j in range(0, precision)])
+def pi(precision):return 4 * sum([(1 - 2 * (j % 2)) / (2 * j + 1) for j in range(0, precision)])
